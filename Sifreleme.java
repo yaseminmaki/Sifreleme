ee4c73e3469f82ad4e2e5061190a192f83acae41
yaseminmaki <84514183+yaseminmaki@users.noreply.github.com>
2023-04-27 22:41:39 +0300

    Update Sifreleme.java

--- Sifreleme.java
+++ Sifreleme.java
@@ -9,6 +9,8 @@ package sifreleme;
  * @author SONY
  */
 import java.util.Scanner;
+import java.util.Base64;
+
 
 public class Sifreleme {
 
@@ -49,7 +51,7 @@ public class Sifreleme {
             for (int i = 0; i < value; i++) {
                 //--number;
                 if (number == 0) {
-                    number = 29;
+                    number = mod;
 
                 }
                 --number;
@@ -70,16 +72,34 @@ public class Sifreleme {
         System.out.println(result);
 
     }
+    public static void base64Encode(String originalString){
 
+
+       // String originalString = "Hello, World!";
+        String encodedString = Base64.getEncoder().encodeToString(originalString.getBytes());
+        System.out.println("Encoded string: " + encodedString);
+ 
+
+    }
+
+    public static void base64Decode(String encodedString){
+        byte[] decodedBytes = Base64.getDecoder().decode(encodedString);
+        String decodedString = new String(decodedBytes);
+        System.out.println("Decoded string: " + decodedString);
+    }
     public static void main(String[] args) {
         // TODO code application logic here
+        
+            //base64Decode("gklMR0lTQVlBUg");
+            //System.exit(0);
+       
         while (true) {
             System.out.println("Enter a number: ");
             Scanner scan = new Scanner(System.in);
             int number = scan.nextInt();
             //sifreCoz(6, 5, number);
             //dogrusalSifreleme(4, 9, number,31);
-            sezarCoz(5, number, 29);
+            sezar(3,number, 26);
             if (number == 0) {
                 break;
             }
